5fbde9b0657e03cfa22ba0336c8d029a8eaba3b0
Teemu Toivola <git@humdi.net>
2018-11-27 21:13:07 +0200

    restore SSD1306 as default display, most likely unintentionally changed in commit 04ed5ae77dc0d0fecfd6e71887713f089e0ec9ed

--- printermonitor/Settings.h
+++ printermonitor/Settings.h
@@ -84,7 +84,7 @@ const int I2C_DISPLAY_ADDRESS = 0x3c; // I2C Address of your Display (usually 0x
 const int SDA_PIN = D2;
 const int SCL_PIN = D5;
 boolean INVERT_DISPLAY = false; // true = pins at top | false = pins at the bottom
-#define DISPLAY_SH1106       // Uncomment this line to use the SH1106 display -- SSD1306 is used by default and is most common
+//#define DISPLAY_SH1106       // Uncomment this line to use the SH1106 display -- SSD1306 is used by default and is most common
 
 boolean ENABLE_OTA = true;     // this will allow you to load firmware to the device over WiFi (see OTA for ESP8266)
 String OTA_Password = "";      // Set an OTA password here -- leave blank if you don't want to be prompted for password
