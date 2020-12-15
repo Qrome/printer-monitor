0be2ccb26deb864587f90ea92fa55f4ad53b0633
Robert Stein <DerStone@hotmail.de>
2020-12-15 18:56:51 +0100

    More webUI improvements

--- src/Network/WebserverMemoryVariables.cpp
+++ src/Network/WebserverMemoryVariables.cpp
@@ -33,59 +33,30 @@ void WebserverMemoryVariables::sendHeader(
     server->setContentLength(CONTENT_LENGTH_UNKNOWN);
     server->send(200, "text/html", "");
 
-    server->sendContent("<!DOCTYPE HTML>"
-        "<html><head><title>PrintBuddy</title><link rel='icon' href='data:;base64,='>"
-        "<meta charset='UTF-8'>"
-        "<meta name='viewport' content='width=device-width, initial-scale=1'>"
-    );
+    server->sendContent(String(FPSTR(HEADER_BLOCK1)));
     if (refresh) {
         server->sendContent("<meta http-equiv=\"refresh\" content=\"30\">");
     }
-    server->sendContent("<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>"
-        "<link rel='stylesheet' href='https://unpkg.com/carbon-components/css/carbon-components.min.css'></style>"
-        "<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.15.1/css/all.css'>"
-        "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>"
-        "<style>.hidden{display:none}</style>"
-        "</head><body>"
-        "<header class='cv-header bx--header'>"
-        "<a href='/' class='cv-header-name bx--header__name'>"
-    );
+    server->sendContent(String(FPSTR(HEADER_BLOCK2)));
     server->sendContent("<span class='bx--header__name--prefix'>PrintBuddy&nbsp;</span>V" + String(VERSION));
-    server->sendContent("</a>"
-        "<nav class='cv-header-nav bx--header__nav'></nav>"
-        "<div class='bx--header__global'>"
-            "<button type='button' class='cv-header-global-action bx--header__action' onclick='openWifiInfo()'>"
-                "<i class='fas fa-wifi' style='color: white; font-size: 20px;'></i>"
-            "</button>"
-            "<button type='button' class='cv-header-global-action bx--header__action' onclick='openSidebar()'>"
-                "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='20' height='20' viewBox='0 0 32 32' aria-hidden='true'>"
-                    "<path d='M14 4H18V8H14zM4 4H8V8H4zM24 4H28V8H24zM14 14H18V18H14zM4 14H8V18H4zM24 14H28V18H24zM14 24H18V28H14zM4 24H8V28H4zM24 24H28V28H24z'></path>"
-                "</svg>"
-            "</button>"
-        "</div>"
-        "<div id='sidebar' class='cv-header-panel bx--header-panel'>"
-            "<ul class='cv-switcher bx--switcher__item'>"
-    );
+    server->sendContent(String(FPSTR(HEADER_BLOCK3)));
     server->sendContent(String(FPSTR(MENUE_ITEMS)));
-    server->sendContent("</ul>"
-        "</div>"
-        "<div class='bx--toast-notification bx--toast-notification--info hidden' style='position: absolute; right: -16px; top: 40px;' id='wifiinfo'>"
-            "<div class='bx--toast-notification__details'>"
-                "<h3 class='bx--toast-notification__title'>WiFi Signal Strength</h3>"
-                    "<div class='bx--toast-notification__subtitle'><span>"
-    );
-    server->sendContent(String(rssi) + "%");
-    server->sendContent("</span></div>"
-                "</div>"
-            "</div>"
-        "</header>"
-        "<script>function openSidebar(){document.getElementById('sidebar').classList.toggle('bx--header-panel--expanded');document.getElementById('wifiinfo').classList.add('hidden');};function openWifiInfo(){document.getElementById('sidebar').classList.remove('bx--header-panel--expanded');document.getElementById('wifiinfo').classList.toggle('hidden');}</script>"
-        "<br><div class='bx--grid bx--grid--full-width' style='margin-top:60px'>"
-            "<div class='page-header' style='margin-bottom:20px'><h4 class='page-header__label'>");
+    server->sendContent(String(FPSTR(HEADER_BLOCK4)));
+
+    uint32_t heapFree = 0;
+    uint16_t heapMax = 0;
+    uint8_t heapFrag = 0;
+    ESP.getHeapStats(&heapFree, &heapMax, &heapFrag);
+
+    server->sendContent("<div>WiFi Signal Strength: " + String(rssi) + "%</div>");
+    server->sendContent("<div>ESP ChipID: " + String(ESP.getChipId()) + "</div>");
+    server->sendContent("<div>ESP CoreVersion: " + String(ESP.getCoreVersion()) + "</div>");
+    server->sendContent("<div>Heap (frag/free/max): " + String(heapFrag) + "|" +  String(heapFree) + "|" + String(heapMax) + "</div>");
+    server->sendContent(String(FPSTR(HEADER_BLOCK5)));
     server->sendContent(pageLabel);
     server->sendContent("</h4><h1 id='page-title' class='page-header__title'>");
     server->sendContent(pageTitle);
-    server->sendContent("</h1></div><div class='bx--row'>");
+    server->sendContent("</h1></div>");
 }
 
 /**
@@ -94,8 +65,7 @@ void WebserverMemoryVariables::sendHeader(
  * @param globalDataController      Access to global data
  */
 void WebserverMemoryVariables::sendFooter(ESP8266WebServer *server, GlobalDataController *globalDataController) {
-    server->sendContent("<br><br><br></div></div>");
-    server->sendContent("<script src='https://unpkg.com/carbon-components/scripts/carbon-components.min.js'></script></body></html>");
+    server->sendContent(String(FPSTR(FOOTER_BLOCK)));
     server->sendContent("");
     server->client().stop();
     globalDataController->ledOnOff(false);
@@ -108,4 +78,107 @@ void WebserverMemoryVariables::sendFooter(ESP8266WebServer *server, GlobalDataCo
  */
 void WebserverMemoryVariables::sendUpdateForm(ESP8266WebServer *server, GlobalDataController *globalDataController) {
     server->sendContent(FPSTR(UPDATE_FORM));
+}
+
+/**
+ * @brief Send out configuration for weather
+ * @param server                    Send out instancce
+ * @param globalDataController      Access to global data
+ */
+void WebserverMemoryVariables::sendWeatherConfigForm(ESP8266WebServer *server, GlobalDataController *globalDataController) {
+    String isWeatherChecked = "";
+    String isMetricChecked = "";
+    if (globalDataController->getWeatherShow()) {
+        isWeatherChecked = "checked='checked'";
+    }
+    if (globalDataController->getWeatherIsMetric()) {
+        isMetricChecked = "checked='checked'";
+    }
+
+    String form = FPSTR(WEATHER_FORM1);
+    form.replace("%IS_WEATHER_CHECKED%", isWeatherChecked);
+    server->sendContent(form);
+
+    form = FPSTR(WEATHER_FORM2);
+    form.replace("%METRIC%", isMetricChecked);
+    server->sendContent(form);
+
+    form = FPSTR(WEATHER_FORM3);
+    form.replace("%WEATHERKEY%", globalDataController->getWeatherApiKey());
+    server->sendContent(form);
+
+    form = FPSTR(WEATHER_FORM4);
+    form.replace("%CITY1%", String(globalDataController->getWeatherCityId()));
+    form.replace("%CITYNAME1%", globalDataController->getWeatherClient()->getCity(0));
+    server->sendContent(form);
+
+    form = FPSTR(WEATHER_FORM_OPTIONS);
+    form.replace(">"+String(globalDataController->getWeatherLang())+"<", " selected>"+String(globalDataController->getWeatherLang())+"<");
+    server->sendContent(form);
+
+    form = FPSTR(WEATHER_FORM5);
+    server->sendContent(form);
+}
+
+/**
+ * @brief Send out configuration for station
+ * @param server                    Send out instancce
+ * @param globalDataController      Access to global data
+ */
+void WebserverMemoryVariables::sendStationConfigForm(ESP8266WebServer *server, GlobalDataController *globalDataController) {
+    String isClockChecked = "";
+    String is24hourChecked = "";
+    String isInvDisp = "";
+    String isFlashLED = "";
+    String isUseSecurityChecked = "";
+    if (globalDataController->isDisplayInverted()) {
+        isInvDisp = "checked='checked'";
+    }
+    if (globalDataController->useLedFlash()) {
+        isFlashLED = "checked='checked'";
+    }
+    if (globalDataController->getDisplayClock()) {
+        isClockChecked = "checked='checked'";
+    }
+    if (globalDataController->getClockIs24h()) {
+        is24hourChecked = "checked='checked'";
+    }
+    if (globalDataController->getWebserverIsBasicAuth()) {
+        isUseSecurityChecked = "checked='checked'";
+    }
+
+    String form = FPSTR(STATION_CONFIG_FORM1);
+    form.replace("%IS_CLOCK_CHECKED%", isClockChecked);
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM2);
+    form.replace("%IS_24HOUR_CHECKED%", is24hourChecked);
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM3);
+    form.replace("%IS_INVDISP_CHECKED%", isInvDisp);
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM4);
+    form.replace("%USEFLASH%", isFlashLED);
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM5);
+    String options = FPSTR(STATION_CONFIG_FORM5OPT);
+    options.replace(">"+String(globalDataController->getClockResyncMinutes())+"<", " selected>"+String(globalDataController->getClockResyncMinutes())+"<");
+    form.replace("%OPTIONS%", options);
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM6);
+    form.replace("%UTCOFFSET%", String(globalDataController->getClockUtcOffset()));
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM7);
+    form.replace("%IS_BASICAUTH_CHECKED%", isUseSecurityChecked);
+    server->sendContent(form);
+
+    form = FPSTR(STATION_CONFIG_FORM8);
+    form.replace("%USERID%", globalDataController->getWebserverUsername());
+    form.replace("%STATIONPASSWORD%", globalDataController->getWebserverPassword());
+    server->sendContent(form);
 }
